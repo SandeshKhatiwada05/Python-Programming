56fa91250534d6b6ff1d559c207d498ede6d5659
SandeshKhatiwada05 <khatiwadasandesh501@gmail.com>
2026-01-19 18:00:51 +0545

    switch case

---
+++ 36.SwitchCase.py
@@ -0,0 +1,15 @@
+print("Enter Number from 1-5")
+n= int(input("Enter :: "))
+match n:
+    case 1:
+        print("Hello I am under water")
+    case 2:
+        print("Hehe Bro") 
+    case 3:
+        print("Avengers Assemble")
+    case 4:
+        print("Where Are you going?")
+    case 5:
+        print("Wakanda Forever")
+    case _:
+        print("J paaye tri number halcha mula")   
